206e5e5d936185cacb050739ac4a427e6d515562
ConnerCleland <Chcleland2@gmail.com>
2023-10-30 09:00:00 -0500

    added anotations

--- codingbat_over_http/project/app/tests.py
+++ codingbat_over_http/project/app/tests.py
@@ -1,50 +1,52 @@
+from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import reverse
+import json
 
 
 class NearHundredTestCase(TestCase):
     def test_near_hundred(self):
         response = self.client.get(reverse("near_hundred", args=[93]))
-        self.assertEqual(response.json()["result"], True)
+        self.assertEqual(json.loads(response.content)["result"], True)
 
         response = self.client.get(reverse("near_hundred", args=[90]))
-        self.assertEqual(response.json()["result"], True)
+        self.assertEqual(json.loads(response.content)["result"], True)
 
         response = self.client.get(reverse("near_hundred", args=[89]))
-        self.assertEqual(response.json()["result"], False)
+        self.assertEqual(json.loads(response.content)["result"], False)
 
 
 class StringSplosionTestCase(TestCase):
     def test_string_splosion(self):
         response = self.client.get(reverse("string_splosion", args=["Code"]))
-        self.assertEqual(response.json()["result"], "CCoCodCode")
+        self.assertEqual(json.loads(response.content)["result"], "CCoCodCode")
 
         response = self.client.get(reverse("string_splosion", args=["abc"]))
-        self.assertEqual(response.json()["result"], "aababc")
+        self.assertEqual(json.loads(response.content)["result"], "aababc")
 
         response = self.client.get(reverse("string_splosion", args=["ab"]))
-        self.assertEqual(response.json()["result"], "aab")
+        self.assertEqual(json.loads(response.content)["result"], "aab")
 
 
 class CatDogTestCase(TestCase):
     def test_cat_dog(self):
         response = self.client.get(reverse("cat_dog", args=["catdog"]))
-        self.assertEqual(response.json()["result"], True)
+        self.assertEqual(json.loads(response.content)["result"], True)
 
         response = self.client.get(reverse("cat_dog", args=["catcat"]))
-        self.assertEqual(response.json()["result"], False)
+        self.assertEqual(json.loads(response.content)["result"], False)
 
         response = self.client.get(reverse("cat_dog", args=["1cat1cadodog"]))
-        self.assertEqual(response.json()["result"], True)
+        self.assertEqual(json.loads(response.content)["result"], True)
 
 
 class LoneSumTestCase(TestCase):
     def test_lone_sum(self):
         response = self.client.get(reverse("lone_sum", args=[1, 2, 3]))
-        self.assertEqual(response.json()["result"], 6)
+        self.assertEqual(json.loads(response.content)["result"], 6)
 
         response = self.client.get(reverse("lone_sum", args=[3, 3, 3]))
-        self.assertEqual(response.json()["result"], 0)
+        self.assertEqual(json.loads(response.content)["result"], 0)
 
         response = self.client.get(reverse("lone_sum", args=[4, 3, 2]))
-        self.assertEqual(response.json()["result"], 9)
+        self.assertEqual(json.loads(response.content)["result"], 9)
--- codingbat_over_http/project/app/views.py
+++ codingbat_over_http/project/app/views.py
@@ -1,28 +1,29 @@
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_GET
+import json
 
 
 @require_GET
-def near_hundred(request, n):
+def near_hundred(request: HttpRequest, n: int) -> HttpResponse:
     result = abs(n - 100) <= 10 or abs(n - 200) <= 10
-    return JsonResponse({"result": result})
+    return HttpResponse(json.dumps({"result": result}), content_type="application/json")
 
 
 @require_GET
-def string_splosion(request, s):
+def string_splosion(request: HttpRequest, s: str) -> HttpResponse:
     result = "".join(s[: i + 1] for i in range(len(s)))
-    return JsonResponse({"result": result})
+    return HttpResponse(json.dumps({"result": result}), content_type="application/json")
 
 
 @require_GET
-def cat_dog(request, s):
+def cat_dog(request: HttpRequest, s: str) -> HttpResponse:
     cat_count = s.count("cat")
     dog_count = s.count("dog")
     result = cat_count == dog_count
-    return JsonResponse({"result": result})
+    return HttpResponse(json.dumps({"result": result}), content_type="application/json")
 
 
 @require_GET
-def lone_sum(request, a, b, c):
+def lone_sum(request: HttpRequest, a: int, b: int, c: int) -> HttpResponse:
     result = a + b + c if a != b and b != c and a != c else 0
-    return JsonResponse({"result": result})
+    return HttpResponse(json.dumps({"result": result}), content_type="application/json")
